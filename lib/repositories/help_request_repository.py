99f9f7ce52e575e21c7f3ba6c09d8dafbe949e9f
aisha <aisha_mo@hotmail.com>
2024-02-27 12:20:09 +0000

    updated help request model test with proper daterange syntax

--- lib/repositories/help_request_repository.py
+++ lib/repositories/help_request_repository.py
@@ -63,3 +63,4 @@ class HelpRequestRepository:
 
         return None
     
+    
--- tests/models/test_help_request.py
+++ tests/models/test_help_request.py
@@ -16,12 +16,12 @@ def  test_constructs_help_request_with_fields():
 
 # test two help request objects are equal
 def test_help_requests_are_equal():
-    help_request_1 = HelpRequest(1, 1, datetime(2024, 2, 1, 10, 30, 45), "Need help with gardening", "Looking for someone to help with planting flowers.", "Rose", "2024-03-01 to 2024-03-07", 100)
-    help_request_2 = HelpRequest(1, 1, datetime(2024, 2, 1, 10, 30, 45), "Need help with gardening", "Looking for someone to help with planting flowers.", "Rose", "2024-03-01 to 2024-03-07", 100)
+    help_request_1 = HelpRequest(1, 1, datetime(2024, 2, 1, 10, 30, 45), "Need help with gardening", "Looking for someone to help with planting flowers.", "Rose", "[2024-01-01, 2024-01-31]", 100)
+    help_request_2 = HelpRequest(1, 1, datetime(2024, 2, 1, 10, 30, 45), "Need help with gardening", "Looking for someone to help with planting flowers.", "Rose", "[2024-01-01, 2024-01-31]", 100)
 
     assert help_request_1 == help_request_2
 
 # test help request object formats nicely with the data provided
 def test_help_request_formats_nicely():
-    help_request = HelpRequest(1, 1, datetime(2024, 2, 1, 10, 30, 45), "Need help with gardening", "Looking for someone to help with planting flowers.", "Rose", "2024-03-01 to 2024-03-07", 100)
-    assert str(help_request) == "HelpRequest(1, 1, 2024-02-01 10:30:45, Need help with gardening, Looking for someone to help with planting flowers., Rose, 2024-03-01 to 2024-03-07, 100)"
+    help_request = HelpRequest(1, 1, datetime(2024, 2, 1, 10, 30, 45), "Need help with gardening", "Looking for someone to help with planting flowers.", "Rose", "[2024-01-01, 2024-01-31]", 100)
+    assert str(help_request) == "HelpRequest(1, 1, 2024-02-01 10:30:45, Need help with gardening, Looking for someone to help with planting flowers., Rose, [2024-01-01, 2024-01-31], 100)"
