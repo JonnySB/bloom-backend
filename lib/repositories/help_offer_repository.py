81fa40ed107401770d8e44ced9ee62537369e8d4
Martynas Prusevicius <mprusevicius@gmail.com>
2024-02-27 10:30:27 +0000

    Add create method for help_offer_repository.py

--- lib/repositories/help_offer_repository.py
+++ lib/repositories/help_offer_repository.py
@@ -11,3 +11,8 @@ class HelpOfferRepository():
             help_offer = HelpOffer(row['id'], row['user_id'], row['message'], row['bid'], row['status'])
             help_offers.append(help_offer)
         return help_offers
+    
+    def create_offer(self, offer):
+        self.connection.execute("INSERT INTO help_offers (user_id, message, bid, status) \
+                                VALUES (%s, %s, %s, %s)",
+                                [offer.user_id, offer.message, offer.bid, offer.status])
