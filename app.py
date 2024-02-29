c2c65683ae221552c58502df25c10ab4129abc48
Martynas Prusevicius <mprusevicius@gmail.com>
2024-02-29 08:58:52 +0000

    Add GET /help_offers/help_requests/<user_id> route

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 from lib.repositories.user_repository import UserRepository
 from lib.repositories.help_offer_repository import HelpOfferRepository
 from lib.models.help_offer import HelpOffer
+from lib.repositories.help_request_repository import HelpRequestRepository
 
 # load .env file variables see readme details
 load_dotenv()
@@ -76,6 +77,28 @@ def create_help_offer(help_request_id):
     new_offer = HelpOffer(None, user_id, request_id, message, bid, status)
     offer_repository.create_offer(new_offer)
     return 201
+
+#return array of offer IDs for requests made by user
+@app.route("/help_offers/help_requests/<user_id>", methods=["GET"])
+def help_offered_to_user(user_id):
+
+    #connect to db and set up offer repository
+    connection = get_flask_database_connection(app)
+    request_repository = HelpRequestRepository(connection)
+    offer_repository = HelpOfferRepository(connection)
+
+    #get IDs of help requests made by user
+    requests_by_user = request_repository.find_requests_by_user_id(user_id)
+    
+    
+    #get IDs of offers matching user help requests
+    help_offered_to_user = []
+    for request in requests_by_user:
+        offers_for_request = offer_repository.find_by_request_id(request.id)
+        for offer in offers_for_request:
+            help_offered_to_user.append(offer.id)
+    return jsonify({"help_offered_to_user": help_offered_to_user})
+    
 
 if __name__ == "__main__":
     app.run(debug=True, port=int(os.environ.get("PORT", 5001)))
