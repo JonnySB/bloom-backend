90617bdb4ed42104f0c72c7e2dd87058f8754904
aisha <aisha_mo@hotmail.com>
2024-02-29 10:29:07 +0000

    removed redundant import

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, jsonify, request
 from lib.database_connection import get_flask_database_connection
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required
 from dotenv import load_dotenv
 
 from lib.repositories.user_repository import UserRepository
