d6cfa8e7458c34bfe44cd21b64494c8437def346
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-03-01 14:12:10 +0000

    changes in the logic

--- lib/repositories/chat_repository.py
+++ lib/repositories/chat_repository.py
@@ -1,5 +1,5 @@
 from lib.models.chat import Chat
-import datetime
+from datetime import datetime, timedelta
 import json
 
 class ChatRepository:
@@ -20,26 +20,31 @@ class ChatRepository:
         # the logic is we will only show messages that are 30 days old to avoid creating multiple messages in the database. if there is a chat between the users within the last 30 days, then we will append the new message to an array,
         #otherwise we will create a new message
 
-    def create(self,receiver, sender):
-        start_date = datetime.datetime.now() - datetime.timedelta(days=30)  # Calculate date 30 days ago from now
+
+
+    def create(self, receiver, sender):
+        # Today's date for comparison
+        today_str = datetime.now().strftime('%Y-%m-%d')
+
+        # Calculate dates 30 days before and after today
+        start_date = datetime.now() - timedelta(days=30)
         start_date_str = start_date.strftime('%Y-%m-%d')
-        end_date = start_date + datetime.timedelta(days=30)  # Calculate date 30 days ago from now
+        end_date = datetime.now() + timedelta(days=30)  # If you're setting a future end_date based on today
         end_date_str = end_date.strftime('%Y-%m-%d')
-        
-        
-        # Check for existing chat between the recipient and sender in the last 30 days
-        query = '''SELECT * FROM chats WHERE recipient_id = %s AND sender_id = %s AND start_date >= %s AND end_date <= %s'''
-        existing_chat = self._connection.execute(query, [receiver.id, sender.id, start_date_str, end_date_str])
-        
+
+        # Check for existing chat between the recipient and sender where today is within the start and end dates
+        query = '''SELECT * FROM chats WHERE recipient_id = %s AND sender_id = %s AND %s BETWEEN start_date AND end_date'''
+        existing_chat = self._connection.execute(query, [receiver.id, sender.id, today_str])
+
         if not existing_chat:
             # If no messages in the last 30 days, insert the new message
             insert_query = '''INSERT INTO chats (recipient_id, message, start_date, end_date, sender_id) VALUES (%s, ARRAY[%s], %s, %s, %s) RETURNING *;'''
             return self._connection.execute(insert_query, [receiver.id, receiver.message, start_date_str, end_date_str, sender.id])
         else:
-            # If there is an existing chat, append the new message to the existing 'message' field.
-            # it is splited down by a comma so we can uset the split method in the future 
-            update_query = '''UPDATE chats SET message = array_append(message, %s) WHERE recipient_id = %s AND sender_id = %s AND start_date = %s  AND end_date = %s RETURNING *;'''
-            return self._connection.execute(update_query, [receiver.message, receiver.id,  start_date_str, end_date_str,  sender.id,])
+            # If there is an existing chat, append the new message to the existing 'message' field
+            update_query = '''UPDATE chats SET message = array_append(message, %s) WHERE recipient_id = %s AND sender_id = %s AND %s BETWEEN start_date AND end_date RETURNING *;'''
+            return self._connection.execute(update_query, [receiver.message, receiver.id, sender.id, today_str])
+
 
 
        
