0ef5a81c1ab6ad8fb273f812532655be1a620331
Jonny Brownrigg <jonny-uk@hotmail.com>
2024-03-07 17:21:29 +0000

    sorted out seed

--- seeds/bloom.sql
+++ seeds/bloom.sql
@@ -43,17 +43,20 @@ CREATE TABLE user_plants (
     CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id),
     CONSTRAINT fk_plant FOREIGN KEY (plant_id) REFERENCES plants (id)
 );
--- Create sequence for chats
+
 CREATE SEQUENCE chats_id_seq;
 -- Create chats table
 CREATE TABLE chats (
     id SERIAL PRIMARY KEY,
-    received_from VARCHAR(255),
-    sent_to VARCHAR(255),
-    message TEXT,
-    date TIMESTAMP WITHOUT TIME ZONE,
-    user_id INT NOT NULL,
-    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id)
+    recipient_id INT NOT NULL, 
+    message TEXT[],
+    start_date TIMESTAMP WITHOUT TIME ZONE,
+    end_date TIMESTAMP WITHOUT TIME ZONE,
+    receiver_username VARCHAR(255),
+    sender_username VARCHAR(255),
+    sender_id INT NOT NULL, 
+    CONSTRAINT fk_user_sender FOREIGN KEY (sender_id) REFERENCES users (id),
+    CONSTRAINT fk_user_recipient FOREIGN KEY (recipient_id) REFERENCES users (id)
 );
 -- Create sequence for help_request
 CREATE SEQUENCE help_request_id_seq;
@@ -68,16 +71,18 @@ CREATE TABLE help_request (
     maxprice MONEY NOT NULL,
     CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id)
 );
--- Create sequence for help_offers
+
 CREATE SEQUENCE help_offers_id_seq;
 -- Create help_offers table
 CREATE TABLE help_offers (
     id SERIAL PRIMARY KEY,
-    message TEXT NOT NULL,
-    status VARCHAR(255) NOT NULL,
-    user_id INT NOT NULL,
-    bid MONEY NOT NULL,
-    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id)
+    message VARCHAR(255),
+    status VARCHAR(255),
+    user_id INT,
+    request_id INT,
+    bid REAL,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id),
+    CONSTRAINT fk_request FOREIGN KEY (request_id) REFERENCES help_requests (id) ON DELETE CASCADE
 );
 -- CREATE USER SEED --
 INSERT INTO users (first_name, last_name, username, email, hashed_password, avatar_url_string, address) VALUES ('user', '1', 'user1', 'user1@email.com', '$2b$12$6Og77D1E.ObtWMOX9dw//.AJpsNFAR6/6E1OHputlDCUFRytgEQGq', 'https://res.cloudinary.com/dououppib/image/upload/v1708633707/MY_UPLOADS/aibxzxdpk6gl4u5xjgjg.jpg', 'test_address1');
@@ -96,6 +101,15 @@ INSERT INTO "public"."plants" ("common_name", "latin_name", "photo", "watering_f
 -- CREATE USER PLANTS SEED --
 INSERT INTO user_plants (user_id, plant_id, quantity) VALUES (1, 1, 3);
 INSERT INTO user_plants (user_id, plant_id, quantity) VALUES (1, 2, 2);
-INSERT INTO chats (received_from, sent_to, message, date, user_id) VALUES ('user_01', 'user_02', '[hello user 01], [hello user 01]', '2023-10-19', 1);
-INSERT INTO help_request (date, title, message, daterange, user_id, maxprice) VALUES ('2023-10-19 10:23:54', 'title_01', 'message requesting help', '[2023-02-01, 2023-03-01]', 1, 50);
-INSERT INTO help_offers (message, status, user_id, bid) VALUES ('Offering help', 'pending', 1, '50');
+
+INSERT INTO chats (recipient_id, message, start_date, end_date, receiver_username, sender_username, sender_id) VALUES (2, '{"{\"sender\": \"user1\", \"message\": \"Hello user two\"}"}', '2024-01-31','2024-03-01', 'user2', 'user1', 1);
+-- INSERT INTO chats (recipient_id, message, start_date, end_date, receiver_username, sender_username, sender_id) VALUES (3, '{"{\"sender\": \"user2\", \"message\": \"Hello user 01\"}"}',  '2024-01-31','2024-03-01', 'user01', 'user02', 4);
+-- INSERT INTO chats (recipient_id, message, date, sender_id) VALUES (2, '{"Hello user 02 one more time"}', NOW(), 1);
+
+
+INSERT INTO help_requests (date, title, message, start_date, end_date, user_id, maxprice) VALUES ('2023-10-19 10:23:54', 'title_01', 'message requesting help', '2023-02-01', '2023-03-01', 1, 50);
+INSERT INTO help_requests (date, title, message, start_date, end_date, user_id, maxprice) VALUES ('2023-10-20 10:23:54', 'title_02', 'message requesting help 2', '2023-02-03', '2023-03-03', 2, 60);
+INSERT INTO help_requests (date, title, message, start_date, end_date, user_id, maxprice) VALUES ('2023-10-19 10:23:54', 't_03', 'message requesting help 3', '2023-02-01', '2023-03-01', 1, 80);
+INSERT INTO help_offers (message, status, user_id, request_id, bid) VALUES ('Offering help', 'pending', 1, 1, '50');
+INSERT INTO help_offers (message, status, user_id, request_id, bid) VALUES ('I am super expensive', 'pending', 2, 1, '100');
+INSERT INTO help_offers (message, status, user_id, request_id, bid) VALUES ('I am mid priced!', 'pending', 3, 1, '75');
