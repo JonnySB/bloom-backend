12d0f17d3fa728618d95f62a77a802bd93337914
Jonny Brownrigg <jonny-uk@hotmail.com>
2024-03-12 19:26:38 +0000

    adds extended help offer model tests

--- lib/models/extended_help_offer.py
+++ lib/models/extended_help_offer.py
@@ -46,4 +46,4 @@ class ExtendedHelpOffer:
 
     # UNTESTED
     def __repr__(self):
-        return f"ExtendedHelpOffer: {self.help_request_id}, {self.help_request_start_date}, {self.help_request_end_date}, {self.help_request_name}, {self.help_request_user_id}, {self.help_offer_id}, {self.help_offer_message}, {self.help_offer_status}, {self.help_offer_user_id}, {self.help_offer_bid}, {self.help_offer_first_name}, {self.help_offer_last_name}, {self.help_offer_username}, {self.help_offer_avatar_url_string})"
+        return f"ExtendedHelpOffer: {self.help_request_id}, {self.help_request_start_date}, {self.help_request_end_date}, {self.help_request_name}, {self.help_request_user_id}, {self.help_offer_id}, {self.help_offer_message}, {self.help_offer_status}, {self.help_offer_user_id}, {self.help_offer_bid}, {self.help_offer_first_name}, {self.help_offer_last_name}, {self.help_offer_username}, {self.help_offer_avatar_url_string}, {self.help_receive_first_name}, {self.help_receive_last_name}, {self.help_receive_username}, {self.help_receive_avatar_url_string}"
